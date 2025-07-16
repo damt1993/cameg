d5f32a4b830aeabc862cf8641c2c14eb77c7338a
Richard Damgale <damt1993@gmail.com>
2025-07-16 22:23:30 +0000

    First day profil

---
+++ src/Entity/Collaborator.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CollaboratorRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: CollaboratorRepository::class)]
+class Collaborator
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[Assert\NotBlank(message: "Veuillez entrer le nom complet")]
+    #[Assert\NotNull(message: "Ce champs ne peut pas être vide")]
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[Assert\NotBlank()]
+    #[Assert\NotNull()]
+    #[ORM\Column(length: 255)]
+    private ?string $telephone = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $lastConnectedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): static
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getLastConnectedAt(): ?\DateTimeImmutable
+    {
+        return $this->lastConnectedAt;
+    }
+
+    public function setLastConnectedAt(\DateTimeImmutable $lastConnectedAt): static
+    {
+        $this->lastConnectedAt = $lastConnectedAt;
+
+        return $this;
+    }
+}
